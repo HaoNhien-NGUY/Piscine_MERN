0ebb725e8a0870d985f752e03e353fa5e669f625
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-05-04 21:51:51 +0200

    ex6 done

--- J1/ex_06/my_MERN_module.js
+++ J1/ex_06/my_MERN_module.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 exports.create = (name) => {
-    return  fs.writeFile(name, '', function (err) {
+    return  fs.writeFile(name, '', (err) => {
         err ? console.log('Create "'+name+'" : KO') : console.log('Create "'+name+'" : OK');
       });
 };
@@ -11,3 +11,15 @@ exports.read = (name) => {
         err ? console.log('Read "'+name+'" : KO') : console.log(data);
       });
 };
+
+exports.update = (name, content) => {
+    return  fs.writeFile(name, content, (err) => {
+        err ? console.log('Update "'+name+'" : KO') : console.log('Update "'+name+'" : OK');
+      });
+}
+
+exports.delete = (name) => {
+    return  fs.unlink(name, (err) => {
+        err ? console.log('Delete "'+name+'" : KO') : console.log('Delete "'+name+'" : OK');
+      });
+}
--- J1/ex_06/server.js
+++
@@ -1,5 +0,0 @@
-var myMERN_module = require('./my_MERN_module.js');
-
-// myMERN_module.create('Tom');
-
-myMERN_module.read('Tom');
