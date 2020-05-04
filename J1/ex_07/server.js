3c27ebf51aae1c383ff4904f7f6e80f0417cb5d1
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-05-04 23:21:19 +0200

    test reject

--- J1/ex_07/server.js
+++ J1/ex_07/server.js
@@ -6,25 +6,30 @@ process.env.NODE_ENV = "development";
 
 var app = express();
 
-app.get('/files/:name', async function(req, res) {
-    content = await myMERN_module.read(req.params.name);
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(content);
+app.get('/files/:name', async function (req, res) {
+    myMERN_module.read(req.params.name).then(function (content) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.send(content);
+    }).catch(function () {
+        res.setHeader('Content-Type', 'text/plain');
+        res.send('error');
+    });
 });
 
-app.post('/files/:name', async function(req, res) {
+
+app.post('/files/:name', async function (req, res) {
     content = await myMERN_module.create(req.params.name);
     res.setHeader('Content-Type', 'text/plain');
     res.send(content);
 });
 
-app.put('/files/:name/:content', async function(req, res) {
+app.put('/files/:name/:content', async function (req, res) {
     content = await myMERN_module.update(req.params.name, req.params.content);
     res.setHeader('Content-Type', 'text/plain');
     res.send(content);
 });
 
-app.delete('/files/:name', async function(req, res) {
+app.delete('/files/:name', async function (req, res) {
     content = await myMERN_module.delete(req.params.name);
     res.setHeader('Content-Type', 'text/plain');
     res.send(content);
