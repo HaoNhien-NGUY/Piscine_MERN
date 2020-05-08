4ed4fa714b1d9225d851c45adfb3f003d005bd99
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-05-08 23:18:22 +0200

    react app

--- J4/ex_02/models/categoryModel.js
+++
@@ -1,7 +0,0 @@
-const moogose = require('mongoose');
-
-const categorieSchema = new moogose.Schema({
-    name: {type: String, required: true}
-});
-
-module.exports = moogose.model('categories', categorieSchema);
